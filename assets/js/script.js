eeef68cb96e15d0215e605fcc5c44bc6d8ab6e5a
hannahashe <hannahashe13@gmail.com>
2026-01-11 14:41:17 +0000

    Clear answer box before starting a new game and update page title to "Love Maths"

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
  */
 
 function runGame(gameType) {
+  document.getElementById("answer-box").value = "";
   // creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
